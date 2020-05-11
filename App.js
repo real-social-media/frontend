4945cf9ae6a7408bb48b4e95759a1efeb42c76f9
Azim Gadzhiagayev <me@azimgd.com>
2020-05-11 22:49:37 +0500

    Remove code-push;

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { NavigationContainer } from '@react-navigation/native'
 import { AuthProvider } from 'services/providers/App'
 import { Provider as PaperProvider } from 'react-native-paper'
-import codePush from 'react-native-code-push' 
 import { ThemesContext } from 'navigation/context'
 import AuthNavigator from 'navigation/AuthNavigator'
 import AppNavigator from 'navigation/AppNavigator'
@@ -13,7 +12,6 @@ import store, { persistor } from 'store/index'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'services/Logger'
-import Config from 'react-native-config' 
 import { enableScreens } from 'react-native-screens'
 import PinchZoomComponent from 'components/PostsList/PinchZoom'
 import PostsListContextComponent from 'components/PostsList/Context'
@@ -22,26 +20,6 @@ import UIContextComponent from 'components/UI/Context'
 enableScreens()
 
 dayjs.extend(relativeTime)
-
-const codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  installMode: codePush.InstallMode.IMMEDIATE,
-  mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
-  deploymentKey: Config.CODE_PUSH_DEPLOYMENT_KEY,
-  codePushStatusDidChange,
-  codePushDownloadDidProgress,
-}
-
-codePush.sync(codePushOptions)
-
-const codePushStatusDidChange = (syncStatus) => {
-  if (syncStatus === codePush.SyncStatus.INSTALLING_UPDATE) {
-  } else {
-  }
-}
-
-const codePushDownloadDidProgress = () => {
-}
 
 if (Text.defaultProps == null) {
   Text.defaultProps = Text.defaultProps || {}
@@ -116,4 +94,4 @@ const App = () => {
   )
 }
 
-export default codePush(codePushOptions)(App)
+export default App
