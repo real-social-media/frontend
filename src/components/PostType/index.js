dfadd2110f3c4338fb2f374c04665267082ba4e6
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-08-27 17:50:38 +0300

    Implement new flow to learn users make verified posts

--- src/components/PostType/index.js
+++ src/components/PostType/index.js
@@ -1,58 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  TouchableOpacity,
-  SafeAreaView,
-  StyleSheet,
-  View,
-} from 'react-native'
+import { TouchableOpacity, SafeAreaView, StyleSheet, View } from 'react-native'
 import TextIcon from 'assets/svg/types/Text'
 import PhotoIcon from 'assets/svg/types/Photo'
 import GalleryIcon from 'assets/svg/types/Gallery'
 import { Subheading } from 'react-native-paper'
-import * as navigationActions from 'navigation/actions'
 import color from 'color'
 
 import { withTheme } from 'react-native-paper'
-import { useNavigation } from '@react-navigation/native'
 import { withTranslation } from 'react-i18next'
+import testIDs from './test-ids'
 
-const PostType = ({
-  t,
-  theme,
-  handleLibrarySnap,
-}) => {
+const PostType = ({ t, theme, handleLibrarySnap, handlePhotoTab, handleTextPostTab, handleClose }) => {
   const styling = styles(theme)
-  const navigation = useNavigation()
 
   return (
     <View style={styling.root}>
-      <TouchableOpacity style={styling.backdrop} onPress={navigationActions.navigateBack(navigation)} />
-    
+      <TouchableOpacity testID={testIDs.backdrop} style={styling.backdrop} onPress={handleClose} />
+
       <SafeAreaView style={styling.component}>
         <View style={styling.types}>
-          <TouchableOpacity style={styling.type} onPress={() => {
-            navigation.popToTop()
-            navigationActions.navigateCamera(navigation)()
-          }}>
+          <TouchableOpacity style={styling.type} onPress={handlePhotoTab}>
             <View style={styling.icon}>
               <PhotoIcon fill={theme.colors.backgroundSecondary} />
             </View>
             <Subheading style={styling.text}>{t('Photo')}</Subheading>
           </TouchableOpacity>
-          <TouchableOpacity style={styling.type} onPress={() => {
-            navigation.popToTop()
-            handleLibrarySnap(true)
-          }}>
+          <TouchableOpacity style={styling.type} onPress={handleLibrarySnap}>
             <View style={styling.icon}>
               <GalleryIcon fill={theme.colors.backgroundSecondary} />
             </View>
             <Subheading style={styling.text}>{t('Gallery')}</Subheading>
           </TouchableOpacity>
-          <TouchableOpacity style={styling.type} onPress={() => {
-            navigation.popToTop()
-            navigationActions.navigatePostCreate(navigation, { type: 'TEXT_ONLY' })()
-          }}>
+          <TouchableOpacity style={styling.type} onPress={handleTextPostTab}>
             <View style={styling.icon}>
               <TextIcon fill={theme.colors.backgroundSecondary} />
             </View>
@@ -60,10 +40,7 @@ const PostType = ({
           </TouchableOpacity>
         </View>
 
-        <TouchableOpacity style={styling.close} onPress={() => {
-          navigation.popToTop()
-          navigationActions.navigateBack(navigation)()
-        }}>
+        <TouchableOpacity style={styling.close} onPress={handleClose}>
           <Subheading style={styling.closeText}>{t('x close')}</Subheading>
         </TouchableOpacity>
       </SafeAreaView>
@@ -71,63 +48,67 @@ const PostType = ({
   )
 }
 
-const styles = theme => StyleSheet.create({
-  root: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  backdrop: {
-    ...StyleSheet.absoluteFill,
-  },
-  gradient: {
-    ...StyleSheet.absoluteFill,
-  },
-  component: {
-    height: 180,
-    borderRadius: 24,
-    backgroundColor: theme.colors.backgroundSecondary,
-  },
-  types: {
-    zIndex: 2,
-    maxWidth: 300,
-    flexDirection: 'row',
-    alignSelf: 'center',
-    paddingTop: theme.spacing.base * 2,
-    paddingBottom: theme.spacing.base,
-  },
-  type: {
-    paddingHorizontal: theme.spacing.base,
-  },
-  close: {
-    zIndex: 2,
-    alignSelf: 'center',
-    paddingHorizontal: theme.spacing.base * 2,
-  },
-  icon: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    backgroundColor: theme.colors.border,
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden',
-  },
-  text: {
-    color: color(theme.colors.text).fade(.4),
-    fontWeight: '500',
-    alignSelf: 'center',
-    fontSize: 14,
-  },
-  closeText: {
-    color: theme.colors.text,
-    fontSize: 14,
-  },
-})
+const styles = (theme) =>
+  StyleSheet.create({
+    root: {
+      flex: 1,
+      justifyContent: 'flex-end',
+    },
+    backdrop: {
+      ...StyleSheet.absoluteFill,
+    },
+    gradient: {
+      ...StyleSheet.absoluteFill,
+    },
+    component: {
+      height: 180,
+      borderRadius: 24,
+      backgroundColor: theme.colors.backgroundSecondary,
+    },
+    types: {
+      zIndex: 2,
+      maxWidth: 300,
+      flexDirection: 'row',
+      alignSelf: 'center',
+      paddingTop: theme.spacing.base * 2,
+      paddingBottom: theme.spacing.base,
+    },
+    type: {
+      paddingHorizontal: theme.spacing.base,
+    },
+    close: {
+      zIndex: 2,
+      alignSelf: 'center',
+      paddingHorizontal: theme.spacing.base * 2,
+    },
+    icon: {
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+      backgroundColor: theme.colors.border,
+      justifyContent: 'center',
+      alignItems: 'center',
+      overflow: 'hidden',
+    },
+    text: {
+      color: color(theme.colors.text).fade(0.4),
+      fontWeight: '500',
+      alignSelf: 'center',
+      fontSize: 14,
+    },
+    closeText: {
+      color: theme.colors.text,
+      fontSize: 14,
+    },
+  })
 
 PostType.propTypes = {
   theme: PropTypes.any,
   t: PropTypes.any,
-  handleLibrarySnap: PropTypes.any,
+  handleLibrarySnap: PropTypes.func,
+  handlePhotoTab: PropTypes.func,
+  handleTextPostTab: PropTypes.func,
+  handleClose: PropTypes.func,
 }
 
 export default withTranslation()(withTheme(PostType))
