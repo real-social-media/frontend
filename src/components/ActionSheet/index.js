21cdf2ed51fc02211c6fe4db81a99456763dc3ed
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-09-07 14:07:46 +0300

    Add isVisible option for ActionSheet component

--- src/components/ActionSheet/index.js
+++ src/components/ActionSheet/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import RNActionSheet from 'react-native-actionsheet'
+import propOr from 'ramda/src/propOr'
 
-const ActionSheet = ({ actionSheetRef, showDestructive, ...props }) => {
-  const options = showDestructive ? props.options : props.options.filter((i) => !i.isDestructive)
+const isVisible = propOr(true, 'isVisible')
+const undefinedByDefault = i => i === -1 ? undefined : i
+
+const ActionSheet = ({ actionSheetRef, ...props }) => {
+  const options = props.options.filter(isVisible)
   const destructiveButtonIndex = options.findIndex((i) => i.isDestructive)
   const cancelButtonIndex = options.findIndex((i) => i.isCancel)
 
@@ -11,8 +15,8 @@ const ActionSheet = ({ actionSheetRef, showDestructive, ...props }) => {
     <RNActionSheet
       ref={actionSheetRef}
       options={options.map((i) => i.name)}
-      cancelButtonIndex={cancelButtonIndex}
-      destructiveButtonIndex={destructiveButtonIndex}
+      cancelButtonIndex={undefinedByDefault(cancelButtonIndex)}
+      destructiveButtonIndex={undefinedByDefault(destructiveButtonIndex)}
       onPress={(index) => options[index].onPress()}
     />
   )
@@ -20,19 +24,18 @@ const ActionSheet = ({ actionSheetRef, showDestructive, ...props }) => {
 
 ActionSheet.propTypes = {
   actionSheetRef: PropTypes.any,
-  showDestructive: PropTypes.bool,
   options: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
       onPress: PropTypes.func.isRequired,
       isCancel: PropTypes.bool,
       isDestructive: PropTypes.bool,
+      isVisible: PropTypes.bool,
     }),
   ),
 }
 
 ActionSheet.defaultProps = {
-  showDestructive: false,
   options: [],
 }
 
