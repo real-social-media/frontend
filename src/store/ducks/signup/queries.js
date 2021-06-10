eba1c271a91ba873b26c0dcff1fda669da7577f6
Mike Fogel <mike@real.app>
2021-06-10 16:54:15 -0300

    Remove unused Mutation.createCognitoOnlyUser

--- src/store/ducks/signup/queries.js
+++ src/store/ducks/signup/queries.js
@@ -2,15 +2,6 @@ import {
   userFragment,
 } from 'store/fragments'
 
-export const createCognitoOnlyUser = `
-  mutation createCognitoOnlyUser($username: String!, $fullName: String) {
-    createCognitoOnlyUser(username: $username, fullName: $fullName) {
-      ...userFragment
-    }
-  }
-  ${userFragment}
-`
-
 export const createGoogleUser = `
   mutation createGoogleUser($username: String!, $fullName: String, $googleIdToken: String!) {
     createGoogleUser(username: $username, fullName: $fullName, googleIdToken: $googleIdToken) {
