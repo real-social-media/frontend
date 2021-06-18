151ee44b3d808e80ebec9537853c9b57a2844caf
Aleksandr Omelnitckii <omelniz@gmail.com>
2021-06-18 21:18:03 +0300

    feat: connect ela screen with backend

--- src/components/Post/Header.js
+++ src/components/Post/Header.js
@@ -105,7 +105,7 @@ const Header = ({
         {verificationVisibility ? (
           <TouchableOpacity
             testID={testIDs.header.verificationStatus}
-            onPress={navigationActions.navigateVerification(navigation, { actionType: 'BACK' })}
+            onPress={navigationActions.navigateVerification(navigation, { actionType: 'BACK', urlEla: post.image?.urlEla })}
             style={styling.verification}
           >
             <Caption style={styling.verificationStatus}>{t('unauthenticated')}</Caption>
